eb8f3924afa7cab48fed367520c852e831f9335e
VISHVA1403 <vishva28sep@gmail.com>
2024-03-20 13:45:32 +0530

    average calculater output uploaded

--- averagecalculater/api/views.py
+++ averagecalculater/api/views.py
@@ -15,16 +15,32 @@ from .models import *
 
 @api_view(['GET'])
 def AverageCalculater(request,qualifier):
-    url = f'http://20.244.56.144/numbers/{qualifier}'
-    response = requests.get(url)
-    try :
-        prev_state = cache.get('prev')
-    except:
-        prev_state =[]
-    response_data = response.json()
-    prev_state = cache.get('numbers')
-    print(prev_state)
-    cache.set(response_data.get('numbers'))
+    
     
     if request.method=='GET':
-        return Response(response.json())
+        url = f'http://20.244.56.144/numbers/{qualifier}'
+        response = requests.get(url)
+        try :
+            prev_state = cache.get('prev')
+        except:
+            prev_state =[]
+        response_data = response.json()
+        prev_state = cache.get('prev_state')
+        # cache.set('prev_state',response_data.get('numbers'))
+        numbers = response_data.get('numbers')
+        curr_state = numbers.copy()
+        try:
+            for vall in prev_state:
+                if vall in curr_state:
+                    index = curr_state.index(vall)
+                    curr_state.pop(index)
+        finally:
+            avg = sum(curr_state)/len(curr_state)
+            data = {
+                "numbers":numbers,
+        "windowPrevState":prev_state if prev_state else [],
+                "windowCurrState":curr_state,
+                "avg":avg
+            }
+            cache.set('prev_state',curr_state)
+            return Response(data=data)
