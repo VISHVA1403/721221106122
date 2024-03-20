370444bd24c8501eb275a40a5f7694b2113943a8
VISHVA1403 <vishva28sep@gmail.com>
2024-03-20 13:30:26 +0530

    averagecalculater first

--- averagecalculater/api/views.py
+++ averagecalculater/api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+import requests
+# Create your views here.
+from django.core.cache import cache
+from rest_framework import status
+from rest_framework.response import Response
+from django.http import  JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from .models import *
+
+
+@api_view(['GET'])
+def AverageCalculater(request,qualifier):
+    url = f'http://20.244.56.144/numbers/{qualifier}'
+    response = requests.get(url)
+    try :
+        prev_state = cache.get('prev')
+    except:
+        prev_state =[]
+    response_data = response.json()
+    prev_state = cache.get('numbers')
+    print(prev_state)
+    cache.set(response_data.get('numbers'))
+    
+    if request.method=='GET':
+        return Response(response.json())
--- averagecalculater/averagecalculater/settings.py
+++ averagecalculater/averagecalculater/settings.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+    'rest_framework.authtoken',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
