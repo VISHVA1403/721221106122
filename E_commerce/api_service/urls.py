7fcc142e801061c6c6f2d9d495759245ca9c5d33
VISHVA1403 <vishva28sep@gmail.com>
2024-03-20 12:15:28 +0530

    api check

--- E_commerce/api_service/urls.py
+++ E_commerce/api_service/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products/companies/<str:company>/catogories/<str:productname>/products<str:q>',ProductDetailAPIView),
+    path('products/companies/<str:company>/catogories/<str:productname>/products',ProductDetailAPIView),
 
 ]
--- E_commerce/api_service/views.py
+++ E_commerce/api_service/views.py
@@ -25,19 +25,24 @@ def ProductApiView(request):
         return Response(serializer.data)
 
 @api_view(['GET'])
-def  ProductDetailAPIView(request,company,productname,q):
-    url = f'http:20.244.56.144/products/companies/{company}/catogories/{productname}/products{q}'
+def  ProductDetailAPIView(request,company,productname):
+    top = request.GET.get('top', 10)
+    minPrice = request.GET.get('minPrice', 1)
+    maxPrice = request.GET.get('maxPrice', 10000)
+    url = f'http://20.244.56.144/products/companies/{company}/catogories/{productname}/products'
     headers = {
     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzEwOTEzNjI3LCJpYXQiOjE3MTA5MTMzMjcsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImIwZGY2MWQ4LTE5ZDktNGQwOC04OGVmLTA5ODQwMjExNDgzYiIsInN1YiI6InZpc2h2YTI4c2VwQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImdvTWFydCIsImNsaWVudElEIjoiYjBkZjYxZDgtMTlkOS00ZDA4LTg4ZWYtMDk4NDAyMTE0ODNiIiwiY2xpZW50U2VjcmV0IjoiSVFrZXZhcGVaSnBPYUVxUyIsIm93bmVyTmFtZSI6IlZJU0hWQSIsIm93bmVyRW1haWwiOiJ2aXNodmEyOHNlcEBnbWFpbC5jb20iLCJyb2xsTm8iOiI3MjEyMjExMDYxMjIifQ.saFbc78TDF7VB9x8d5yBq9ra7ldx5lIZw5g_D420hiM',
     'Content-Type': 'application/json'
     }
-    response = response.get(url,headers=headers)
-    if  response.status_code != 200:
-        return
-    else:
-        print("hello")
+    params = {
+        'top':top,
+        'minPrice':minPrice,
+        'maxPrice':maxPrice
+    }
+    response = requests.get(url,headers=headers,params=params)
+    print(response)
     if request.method=='GET':
-        if response.status_code==200:
-            return Response(response.json())
+        if response.status_code!=200:
+            return Response(response)
         else:
             return Response({"error":"Not Found"})
